76fff63b795d719395422e512ba551da55010f2a
Flynn <64368760+flynnforester@users.noreply.github.com>
2020-11-11 14:46:50 -0500

    bitbybitfix

---
+++ projects/bitbybit/assets/js/script.js
@@ -0,0 +1,32 @@
+$(function () {
+
+    // init controller
+    var controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({
+            triggerElement: "#sw",
+            duration: 400, // the scene should last for a scroll distance of 400px
+            offset: 100 // start this scene after scrolling for 100px
+        })
+        .setClassToggle("#table", "show") // add class toggle
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: "#s1",
+            duration: "400%", // the scene should last for a scroll distance of 400% of the height, this will get it to stay in the position
+            offset: 100 // start this scene after scrolling for 100px
+        })
+        .setClassToggle("#mb", "show") // add class toggle
+        .addTo(controller);
+    
+
+    new ScrollMagic.Scene({
+            triggerElement: "#s2",
+            duration: 400, // the scene should last for a scroll distance of 500px
+            offset: 100 // start this scene after scrolling for 100px
+        })
+        .setClassToggle("#ram", "show") // add class toggle
+        .addTo(controller);
+
+
+});
