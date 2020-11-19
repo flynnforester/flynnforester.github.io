28751993bec7174e6cf64dd1e408d927b89b762b
Flynn <64368760+flynnforester@users.noreply.github.com>
2020-11-18 21:03:53 -0500

    final draft

--- projects/bitbybit/assets/js/script.js
+++ projects/bitbybit/assets/js/script.js
@@ -5,8 +5,8 @@ $(function () {
 
     new ScrollMagic.Scene({
             triggerElement: "#sw",
-            duration: "400%", // the scene should last for a scroll distance of 400px
-            offset: 20 // start this scene after scrolling for 100px
+            duration: 700, // the scene should last for a scroll distance of 400px
+            offset: 300 // start this scene after scrolling for 100px
         })
         .setClassToggle("#table", "show") // add class toggle
         .addTo(controller);
@@ -14,77 +14,79 @@ $(function () {
     
     new ScrollMagic.Scene({
             triggerElement: "#s1",
-        //     duration: "400%", // the scene should last for a scroll distance of 400% of the height, this will get it to stay in the position
+             duration: 2200, // the scene should last for a scroll distance of 400% of the height, this will get it to stay in the position
             offset: 300 // start this scene after scrolling for 100px
         })
-        .setPin("#mb")
+        .setPin("#mb", {pushFollowers: false})
         .setClassToggle("#mb", "show") // add class toggle
         .addTo(controller);
     
     new ScrollMagic.Scene({
             triggerElement: "#s1",
-        //    duration: 400, // the scene should last for a scroll distance of 500px
+            duration: 2100, // the scene should last for a scroll distance of 500px
             offset: 400 // start this scene after scrolling for 100px
         })
-        .setPin("#cpu")
+        .setPin("#cpu", {pushFollowers: false})
         .setClassToggle("#cpu", "show") // add class toggle
         .addTo(controller);
 
     new ScrollMagic.Scene({
             triggerElement: "#s2",
-        //    duration: 400, // the scene should last for a scroll distance of 500px
+            duration: 1200, // the scene should last for a scroll distance of 500px
             offset: 300 // start this scene after scrolling for 100px
         })
-        .setPin("#rams")
+        .setPin("#rams", {pushFollowers: false})
         .setClassToggle("#rams", "show") // add class toggle
         .addTo(controller);
 
       new ScrollMagic.Scene({
             triggerElement: "#s3",
-        //    duration: 400, // the scene should last for a scroll distance of 500px
+            duration: 400, // the scene should last for a scroll distance of 500px
             offset: 300 // start this scene after scrolling for 100px
         })
-          .setPin("#cool")
+          .setPin("#cool", {pushFollowers: false})
         .setClassToggle("#cool", "show") // add class toggle
         .addTo(controller);
     
     new ScrollMagic.Scene({
             triggerElement: "#s4",
-            duration: 250, // the scene should last for a scroll distance of 500px
+            duration: 4400, // the scene should last for a scroll distance of 500px
             offset: 300 // start this scene after scrolling for 100px
         })
+        .setPin("#inside", {pushFollowers: false})
         .setClassToggle("#inside", "show") // add class toggle
         .addTo(controller);
     
-    new ScrollMagic.Scene({
-            triggerElement: "#s5",
-      //      duration: 250, // the scene should last for a scroll distance of 500px
-            offset: 300 // start this scene after scrolling for 100px
+     new ScrollMagic.Scene({
+            triggerElement: "#s4",
+            duration: 200, // the scene should last for a scroll distance of 500px
+            offset: 400 // start this scene after scrolling for 100px
         })
-        .setPin("#inside2")
-        .setClassToggle("#inside2", "show") // add class toggle
+        .setPin("#airflow", {pushFollowers: false})
+        .setClassToggle("#airflow", "show") // add class toggle
         .addTo(controller);
     
     new ScrollMagic.Scene({
             triggerElement: "#s5",
-      //      duration: 250, // the scene should last for a scroll distance of 500px
-            offset: 400 // start this scene after scrolling for 100px
+            duration: 3600, // the scene should last for a scroll distance of 500px
+            offset: 100 // start this scene after scrolling for 100px
         })
-        .setPin("#mbt")
+        .setPin("#mbt", {pushFollowers: false})
         .setClassToggle("#mbt", "show") // add class toggle
         .addTo(controller);
     
      new ScrollMagic.Scene({
             triggerElement: "#s6",
-            duration: 125, // the scene should last for a scroll distance of 500px
+            duration: 2750, // the scene should last for a scroll distance of 500px
             offset: 300 // start this scene after scrolling for 100px
         })
+        .setPin("#ssd", {pushFollowers: false})
         .setClassToggle("#ssd", "show") // add class toggle
         .addTo(controller);
     
     new ScrollMagic.Scene({
             triggerElement: "#s6",
-            duration: 125, // the scene should last for a scroll distance of 500px
+            duration: 300, // the scene should last for a scroll distance of 500px
             offset: 300 // start this scene after scrolling for 100px
         })
         .setClassToggle("#hdd", "show") // add class toggle
@@ -92,19 +94,30 @@ $(function () {
 
     new ScrollMagic.Scene({
             triggerElement: "#s7",
-          //  duration: 125, // the scene should last for a scroll distance of 500px
-            offset: 400 // start this scene after scrolling for 100px
+            duration: 1700, // the scene should last for a scroll distance of 500px
+            offset: 300 // start this scene after scrolling for 100px
         })
-        .setPin("#psu")
+        
+        .setPin("#psu", {pushFollowers: false})
         .setClassToggle("#psu", "show") // add class toggle
         .addTo(controller);
     
     new ScrollMagic.Scene({
+            triggerElement: "#s7",
+            duration: 250, // the scene should last for a scroll distance of 500px
+            offset: 600 // start this scene after scrolling for 100px
+        })
+        
+        .setPin("#connect", {pushFollowers: false})
+        .setClassToggle("#connect", "show") // add class toggle
+        .addTo(controller);
+    
+    new ScrollMagic.Scene({
             triggerElement: "#s8",
-            duration: 125, // the scene should last for a scroll distance of 500px
+            duration: 450, // the scene should last for a scroll distance of 500px
             offset: 400 // start this scene after scrolling for 100px
         })
-        .setPin("#graphics")
+        .setPin("#graphics", {pushFollowers: false})
         .setClassToggle("#graphics", "show") // add class toggle
         .addTo(controller);
 
